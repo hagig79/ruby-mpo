8e09de1dc530934f0044c8bb7c973391e9bba879
mudwell <mirainoeve@gmail.com>
2012-02-03 08:30:25 +0900

    SOIチェック

--- mpo.rb
+++ mpo.rb
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
-mpofile = open("HNI_0020.MPO.jpg")
-mpofile.binmode
+mpofile = File.open(ARGV[0], "rb")
+
+# SOI
+file_data = mpofile.read
+if (file_data[0] != 0xff) || (file_data[1] != 0xd8) then
+  puts "Error SOI"
+end
+
